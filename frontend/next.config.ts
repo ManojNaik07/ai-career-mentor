b0b0abcb995720e7ae68c7cab8930258e7aa6e27
ManojNaik07 <amanojnaik980@gmail.com>
2025-09-25 11:13:07 +0530

    Connect frontend to backend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://ai-career-mentor-behg.onrender.com/:path*", // backend Render URL
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+
